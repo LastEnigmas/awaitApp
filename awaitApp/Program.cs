a96e0deb44b6b0afe0c9ce9c68b6bfee7455adad
Mehrshad <lastenigmas@gmail.com>
2022-12-14 16:24:15 +0330

    Add project files.

---
+++ awaitApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace AppNamespace
+{
+    class App
+    {
+        public static async Task Main()
+        {
+            Stopwatch stw = new Stopwatch();
+            stw.Start();
+
+            var myTask = new List<Task> { ReadData() };
+            await Task.WhenAll(myTask);
+
+            stw.Stop();
+
+            Console.Write($"\ntime : {stw.Elapsed.TotalMilliseconds}");
+            
+        }
+
+        public static async Task ReadData()
+        {
+            string data = await File.ReadAllTextAsync("nameData.txt");
+            Console.WriteLine(data);
+        }
+
+    }
+}
